39658f39dd5f528f409f655d60789ebe4f4f5295
Uyen Doan <catuyen99@gmail.com>
2023-04-25 00:26:11 -0500

    added style

--- src/components/JobDetail.jsx
+++ src/components/JobDetail.jsx
@@ -19,6 +19,8 @@ const JobDetail = () => {
 
       setPost(data[0]);
     };
+    getById();
+  }, []);
 
   const getCommentByPostId = async () => {
     const { data, error } = await supabase
@@ -29,7 +31,9 @@ const JobDetail = () => {
     setComments(data);
   };
 
-  getCommentByPostId();
+  useEffect(() => {
+    getCommentByPostId();
+  }, [post]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -46,123 +50,101 @@ const JobDetail = () => {
     setNewComment("");
   };
 
-            
-    const getTimeDifference = (time) => {
-        const today = new Date();
-        const orderDateTime = new Date(time);
+  const getTimeDifference = (time) => {
+    const today = new Date();
+    const orderDateTime = new Date(time);
 
-        const timeDifference = Math.abs(today - orderDateTime);
-        const minutesDifference = Math.floor(timeDifference / 60000);
+    const timeDifference = Math.abs(today - orderDateTime);
+    const minutesDifference = Math.floor(timeDifference / 60000);
 
-        if (minutesDifference >= 60)
-            return (
-                Math.floor(minutesDifference / 60) +
-                (Math.floor(minutesDifference / 60) == 1
-                    ? " hour ago"
-                    : " hours ago")
-            );
-        else if (minutesDifference >= 1440)
-            return (
-                Math.floor(minutesDifference / 1440) +
-                (Math.floor(minutesDifference / 1440) == 1
-                    ? " day ago"
-                    : " days ago")
-            );
-        return minutesDifference == 1
-            ? minutesDifference + " minute ago"
-            : minutesDifference + " minutes ago";
-    };
+    if (minutesDifference >= 60)
+      return (
+        Math.floor(minutesDifference / 60) +
+        (Math.floor(minutesDifference / 60) == 1 ? " hour ago" : " hours ago")
+      );
+    else if (minutesDifference >= 1440)
+      return (
+        Math.floor(minutesDifference / 1440) +
+        (Math.floor(minutesDifference / 1440) == 1 ? " day ago" : " days ago")
+      );
+    return minutesDifference == 1
+      ? minutesDifference + " minute ago"
+      : minutesDifference + " minutes ago";
+  };
 
-    return (
-        <div>
-            {post && (
-                <div className="p-16 md:p-32 font-sans-pro">
-                    <div className="flex flex-col items-start w-full p-32 text-left bg-white border rounded-xl border-neutral-400">
-                        <img
-                            src={post.imageURL}
-                            className="self-center mb-10"
-                        />
-                        <h1 className="mb-5 text-6xl font-extrabold">
-                            {post.title}
-                        </h1>
-                        <div className="flex flex-row space-x-4">
-                            <img
-                                src="../src/img/suitcase.png"
-                                className="mt-2 border border-black rounded-full w-9 h-9"
-                                alt="Website Logo"
-                            />
-                            <div className="flex flex-col text-left">
-                                <span className="text-xl">{"Jane Doe"}</span>
-                                <span className="text-black/70">
-                                    Posted on:{" "}
-                                    {getTimeDifference(post.created_at)}
-                                </span>
-                            </div>
-                        </div>
+  return (
+    <div>
+      {post && (
+        <div className="p-16 md:p-32 font-sans-pro">
+          <div className="flex flex-col items-start w-full p-32 text-left bg-white border rounded-xl border-neutral-400">
+            <img src={post.imageURL} className="self-center mb-10" />
+            <h1 className="mb-5 text-6xl font-extrabold">{post.title}</h1>
+            <div className="flex flex-row space-x-4">
+              <img
+                src="../src/img/suitcase.png"
+                className="mt-2 border border-black rounded-full w-9 h-9"
+                alt="Website Logo"
+              />
+              <div className="flex flex-col text-left">
+                <span className="text-xl">{"Jane Doe"}</span>
+                <span className="text-black/70">
+                  Posted on: {getTimeDifference(post.created_at)}
+                </span>
+              </div>
+            </div>
 
-                        <p className="mt-20 text-xl">{post.content} </p>
+            <p className="mt-20 text-xl">{post.content} </p>
 
-                        <div className="w-full mt-20">
-                            <h3 className="mb-5 text-3xl font-extrabold">
-                                Comments
-                            </h3>
+            <div className="w-full mt-20">
+              <h3 className="mb-5 text-3xl font-extrabold">Comments</h3>
 
-                            <div className="flex flex-row space-x-4">
-                                <img
-                                    src="../src/img/suitcase.png"
-                                    className="mt-2 border border-black rounded-full w-9 h-9"
-                                    alt="Website Logo"
-                                />
-                                <form onSubmit={onSubmit} className="w-full">
-                                    <textarea
-                                        type="text"
-                                        rows="5"
-                                        name="comment"
-                                        className="w-full px-5 py-3 border-2 rounded border-neutral-200"
-                                        placeholder="Insert Comment Here..."
-                                        value={newComment}
-                                        onChange={(e) =>
-                                            setNewComment(e.target.value)
-                                        }
-                                    />
-                                    <button className="flex px-5 py-3 mt-2 mb-20 ml-auto text-white transition ease-in border-2 border-transparent rounded bg-cyan-700 hover:bg-white hover:border-cyan-700 hover:text-cyan-700">
-                                        Submit
-                                    </button>
-                                </form>
-                            </div>
+              <div className="flex flex-row space-x-4">
+                <img
+                  src="../src/img/suitcase.png"
+                  className="mt-2 border border-black rounded-full w-9 h-9"
+                  alt="Website Logo"
+                />
+                <form onSubmit={onSubmit} className="w-full">
+                  <textarea
+                    type="text"
+                    rows="5"
+                    name="comment"
+                    className="w-full px-5 py-3 border-2 rounded border-neutral-200"
+                    placeholder="Insert Comment Here..."
+                    value={newComment}
+                    onChange={(e) => setNewComment(e.target.value)}
+                  />
+                  <button className="flex px-5 py-3 mt-2 mb-20 ml-auto text-white transition ease-in border-2 border-transparent rounded bg-cyan-700 hover:bg-white hover:border-cyan-700 hover:text-cyan-700">
+                    Submit
+                  </button>
+                </form>
+              </div>
 
-                            {comments &&
-                                comments.map((comment) => (
-                                    <div className="flex flex-row space-x-4">
-                                        <img
-                                            src="../src/img/suitcase.png"
-                                            className="mt-2 border border-black rounded-full w-9 h-9"
-                                            alt="Website Logo"
-                                        />
-                                        <div className="w-full px-5 py-3 mb-3 border-2 rounded border-neutral-200">
-                                            <div className="flex flex-row items-center mb-2 space-x-1">
-                                                <span className="font-extrabold">
-                                                    Sample Username
-                                                </span>
+              {comments &&
+                comments.map((comment) => (
+                  <div className="flex flex-row space-x-4">
+                    <img
+                      src="../src/img/suitcase.png"
+                      className="mt-2 border border-black rounded-full w-9 h-9"
+                      alt="Website Logo"
+                    />
+                    <div className="w-full px-5 py-3 mb-3 border-2 rounded border-neutral-200">
+                      <div className="flex flex-row items-center mb-2 space-x-1">
+                        <span className="font-extrabold">Sample Username</span>
 
-                                                <RxDotFilled />
+                        <RxDotFilled />
 
-                                                <span className="text-black/70">
-                                                    {getTimeDifference(
-                                                        comment.created_at
-                                                    )}
-                                                </span>
-                                            </div>
+                        <span className="text-black/70">
+                          {getTimeDifference(comment.created_at)}
+                        </span>
+                      </div>
 
-                                            <p>{comment.comment}</p>
-                                            <p></p>
-                                        </div>
-                                    </div>
-                                ))}
-                        </div>
+                      <p>{comment.comment}</p>
+                      <p></p>
                     </div>
-                </div>
-              ))}
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       )}
